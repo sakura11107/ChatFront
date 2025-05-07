97fe1587f7ea6f486b466fffe75674105c9f057b
sakura11107 <ltf1409417317@Gmail.com>
2025-05-07 18:03:29 +0800

    feat 优化导航布局

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
+  <Navbar></Navbar>
   <router-view></router-view>
 </template>
 
 <script setup lang="ts">
 
+import Navbar from "./components/Navbar.vue";
 </script>
 
 <style scoped>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,10 @@
 <template>
-  <Navbar></Navbar>
   <el-button @click="gotoChat">进入聊天室</el-button>
   <el-button @click="gotoTest">测试</el-button>
 </template>
 
 <script setup lang="ts">
   import router from "../router/router.ts";
-  import Navbar from "../components/Navbar.vue";
 
   const gotoChat = () =>{
     //进入Chat.vue
