2791af257b68e9a0eb36d8e2f88173cbf9944790
sakura11107 <ltf1409417317@Gmail.com>
2025-05-15 14:18:49 +0800

    feat 尝试一下代码定位工具

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { codeInspectorPlugin } from 'code-inspector-plugin';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    codeInspectorPlugin({
+      bundler: 'vite',
+      editor: 'code',
+    }),
+  ],
   define:{
-    global:'window'
+    global:'window',
   }
 })
